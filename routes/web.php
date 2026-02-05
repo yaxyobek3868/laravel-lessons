77dde1128c2494c320480d32210ce37cce2be750
macbooki92019 <mismoil0422@gmail.com>
2026-02-05 23:38:51 +0500

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\GroupController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('user', UserController::class);
 Route::resource('group', GroupController::class);
