a80e41c435acbcbfe4e1d21fb76207c578fd2107
yaxyobek3868 <mahmudov3868@gmail.com>
2026-02-07 13:18:56 +0500

    route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,31 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 
-Route::resource('/users', UserController::class);
+Route::middleware('guest')->group(function () {
+
+    Route::get('/', function () {
+        return redirect()->route('register');
+    });
+
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/', function () {
+        return redirect()->route('users.index');
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::resource('users', UserController::class);
+});
